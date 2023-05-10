1cfa697c867d57d964244ad352fa573e98910104
Sam Sipe <sam@sipe.io>
2023-05-10 13:59:53 -0400

    update pins, output group temp, fix time sync

--- src/Espresso.ino
+++ src/Espresso.ino
@@ -2,7 +2,7 @@
  * Project: Espresso
  * Description: Control Espresso Machine from HomeBridge and Apple HomeKit
  * Author: Sam Sipe
- * Date: 01 Aug 19
+ * Date: 10 May 2023
  */
 
 // Includes
@@ -12,14 +12,14 @@
 #include "pid.h"
 
 // Inits
-int powerRelay = D0; // Power Relay
-int boilerRelay = D1; // Boiler Relay
+int powerRelay = D4;  // Power Relay
+int boilerRelay = D6; // Boiler Relay
 
-OneWire oneWire(D2); // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(D2);                // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 DallasTemperature dallas(&oneWire); // Pass oneWire reference to Dallas Temperature.
-double caseTemp; // Here we are declaring the integer variable analogvalue, which we will use later to store the value of the tempSensor.
-double groupTemp; // Temperature of the Espresso Machine's group head
-double tempLoop; // For temp change detection
+double caseTemp;                    // Here we are declaring the integer variable analog value, which we will use later to store the value of the tempSensor.
+double groupTemp;                   // Temperature of the Espresso Machine's group head
+double tempLoop;                    // For temp change detection
 int timeLoop = Time.local();
 int timeOn = Time.local(); // For tracking how long the machine has been on
 
@@ -46,45 +46,40 @@ void setup()
     dallas.begin();
     int deviceCount = dallas.getDeviceCount();
     Particle.publish("Temp Sensors Found", String(deviceCount), PRIVATE);
-
 }
 
 // Loop
 void loop()
 {
-    dallas.requestTemperatures();   // Request temperatures
-    groupTemp = dallas.getTempCByIndex(0);  // get the temperature of the Espresso Machine's group head
+    dallas.requestTemperatures();          // Request temperatures
+    groupTemp = dallas.getTempCByIndex(0); // get the temperature of the Espresso Machine's group head
+    caseTemp = dallas.getTempCByIndex(1);  // get the temperature of the Espresso Machine Case
 
-    if (Time.local() - timeLoop >= 10)
+    if (Time.local() - timeLoop >= 3 && digitalRead(powerRelay) == HIGH && abs(groupTemp - tempLoop) > .1) // 3 second delay and 1/10 degree change
     {
-        caseTemp = dallas.getTempCByIndex(1);  // get the temperature of the Espresso Machine Case
+        Particle.publish("Group Temp", String::format("%.2f", groupTemp), PRIVATE);
 
-        do 
-        {
-            Particle.publish("Temperature", String::format("%.1f", caseTemp), PRIVATE);
-            tempLoop = caseTemp;
-        } while (digitalRead(powerRelay) == HIGH && abs(caseTemp - tempLoop) > 1/16);
-
+        tempLoop = groupTemp;
         timeLoop = Time.local();
     }
 
-    if ( (Time.local() - timeOn >= 14400) && (digitalRead(powerRelay) == HIGH) ) // 4 hours shutoff
+    if ((Time.local() - timeOn >= 14400) && (digitalRead(powerRelay) == HIGH)) // 4 hour boiler shutoff
     {
-        timeOn = Time.local();
         digitalWrite(boilerRelay, LOW);
+        Particle.publish("Boiler", "OFF", PRIVATE);
     }
 
     /* Request time synchronization from the Particle Cloud */
-    if (Time.local() == 14000)
+    if (Time.now() % 86400 == 0)
     {
-        Particle.syncTime();    // Request time synchronization from the Particle Device Cloud
-        waitUntil(Particle.syncTimeDone); // Wait until the device receives time from Particle Device Cloud (or connection to Particle Device Cloud is lost)
+        Particle.syncTime();                                  // Request time synchronization from the Particle Device Cloud
+        waitUntil(Particle.syncTimeDone);                     // Wait until the device receives time from Particle Device Cloud (or connection to Particle Device Cloud is lost)
         Log.info("Current time: %s", Time.timeStr().c_str()); // Print current time
         delay(1000);
     }
 }
 
-//Functions
+// Functions
 int espressoPower(String command)
 {
     if (command == "1")
@@ -110,7 +105,6 @@ int espressoPower(String command)
         if (digitalRead(powerRelay) == HIGH)
         {
             digitalWrite(boilerRelay, HIGH);
-            timeOn = Time.local();
             Particle.publish("Espresso", "ON", PRIVATE);
             return 1;
         }
@@ -130,53 +124,3 @@ int espressoPower(String command)
         return -1;
     }
 }
-
-// int boilerToggle(String command)
-// {
-//     if (command=="1")
-//     {
-//         // if (digitalRead(powerRelay) == HIGH)
-//         // {
-//                 digitalWrite(boilerRelay,HIGH);
-//                 Particle.publish("Boiler", "ON", PRIVATE);
-//                 return 1;
-//         // }
-//         // else if (digitalRead(powerRelay) == LOW)
-//         // {
-//         //     digitalWrite(boilerRelay,LOW);
-//         //     Particle.publish("Boiler", "OFF", PRIVATE);
-//         //     return 0;
-//         // }
-//         // else
-//         // {
-//         //     return -1;
-//         // }
-//     }
-//     else if (command=="0")
-//     {
-//         digitalWrite(boilerRelay,LOW);
-//         Particle.publish("Boiler", "OFF", PRIVATE);
-//         return 0;
-//     }
-//     else if (command=="?")
-//     {
-//         if (digitalRead(boilerRelay) == HIGH)
-//         {
-//             Particle.publish("Boiler", "ON", PRIVATE);
-//             return 1;
-//         }
-//         else if (digitalRead(boilerRelay) == LOW)
-//         {
-//             Particle.publish("Boiler", "OFF", PRIVATE);
-//             return 0;
-//         }
-//         else
-//         {
-//             return -1;
-//         }
-//     }
-//     else
-//     {
-//         return -1;
-//     }
-// }
