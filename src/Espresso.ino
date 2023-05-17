3c3f0cce1c23f18e5d5b247e5597c68d2db603b4
Sam Sipe <sam@sipe.io>
2023-05-16 21:37:25 -0400

    add safety features, wifi reconnect, refactor code

--- src/Espresso.ino
+++ src/Espresso.ino
@@ -2,7 +2,8 @@
  * Project: Espresso
  * Description: Control Espresso Machine from HomeBridge and Apple HomeKit
  * Author: Sam Sipe
- * Date: 10 May 2023
+ * Date: 16 May 2023
+ * Version: 0.3.0
  */
 
 // Includes
@@ -11,41 +12,141 @@
 #include "spark-dallas-temperature.h"
 #include "pid.h"
 
-// Inits
-int powerRelay = D4;  // Power Relay
-int boilerRelay = D6; // Boiler Relay
+SYSTEM_MODE(SEMI_AUTOMATIC);
+SYSTEM_THREAD(ENABLED);
 
-OneWire oneWire(D2);                // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-DallasTemperature dallas(&oneWire); // Pass oneWire reference to Dallas Temperature.
-double caseTemp;                    // Here we are declaring the integer variable analog value, which we will use later to store the value of the tempSensor.
-double groupTemp;                   // Temperature of the Espresso Machine's group head
-double tempLoop;                    // For temp change detection
-int timeLoop = Time.local();
-int timeOn = Time.local(); // For tracking how long the machine has been on
+SerialLogHandler usbLogHandler(LOG_LEVEL_TRACE);
+
+// Pins
+int tempSensor = D2;   // Temperature Sensor
+int powerRelay = D4;   // Power Relay
+int boilerRelay = D6;  // Boiler Relay
+int indicatorLED = D7; // Indicator LED
+
+// Variables
+OneWire oneWire(tempSensor);              // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+DallasTemperature dallas(&oneWire);       // Pass oneWire reference to Dallas Temperature.
+double caseTemp, groupTemp, tempLoop;     // Temperature Variables
+unsigned long timeOn, timeLoop, lastSync; // Time loops
+unsigned long msSeenCloud = 0;            // remember when we last saw the cloud
+static int retries = 0;                   // how many retries to connect are left
+
+#define CLOUD_TIMEOUT 60000 // how many ms are acceptable for lost cloud connection (needs to be > 45sec)
+#define CLOUD_MAXRETRY 5    // how many unsuccessful connection attempts befor a System.reset
+#define SHUTOFF_SEC 14400   // 4 hours in seconds for shut off
+#define SHUTOFF_TEMP 42     // 42ºC for shut off
+#define ONE_DAY_SEC 87000   // 24 hours and 10 minutes in seconds
+
+int timeOnMin()
+{
+    if (digitalRead(powerRelay) == HIGH)
+        return (Time.local() - timeOn) / 60; // Time on in minutes
+    else
+        return 0;
+}
 
 // Setup
 void setup()
 {
+    // Serial and Cloud
+    Serial.begin(115200);
+    Particle.connect();
+
+    // Set times
+    timeOn = Time.local();
+    timeLoop = Time.local();
+    lastSync = Time.local();
+
     // Pin Configs
     pinMode(powerRelay, OUTPUT);
     pinMode(boilerRelay, OUTPUT);
+    pinMode(indicatorLED, OUTPUT);
 
     // Pin States on Startup
     digitalWrite(powerRelay, LOW);
     digitalWrite(boilerRelay, LOW);
+    digitalWrite(indicatorLED, LOW);
 
     // Particle Functions //
     Particle.function("espresso", espressoPower);
-    // Particle.function("boiler", boilerToggle);
 
     // Particle Variables //
     Particle.variable("groupTemp", groupTemp);
     Particle.variable("caseTemp", caseTemp);
+    Particle.variable("timeOn", timeOnMin);
 
     // setup the library
     dallas.begin();
     int deviceCount = dallas.getDeviceCount();
     Particle.publish("Temp Sensors Found", String(deviceCount), PRIVATE);
+}
+
+inline void softDelay(uint32_t msDelay)
+{
+    for (uint32_t ms = millis(); millis() - ms < msDelay; Particle.process())
+        ;
+}
+
+inline void espressoStartup()
+{
+    timeOn = Time.local();
+    Particle.publish("Espresso", "ON", PRIVATE);
+    digitalWrite(powerRelay, HIGH);
+    delay(5000); // smooth startup
+    digitalWrite(boilerRelay, HIGH);
+}
+
+inline void espressoShutoff()
+{
+    Particle.publish("Espresso", "OFF", PRIVATE);
+    digitalWrite(boilerRelay, LOW);
+    delay(5000); // smooth shutdown
+    digitalWrite(powerRelay, LOW);
+}
+
+void maintainWiFi()
+{
+    // check cloud connection and if required reconnect
+    if (Particle.connected())
+    {
+        msSeenCloud = millis();
+        retries = 0;
+    }
+    else if (millis() - msSeenCloud > CLOUD_TIMEOUT)
+    {
+        Particle.disconnect();
+        softDelay(500);
+        WiFi.disconnect();
+        softDelay(1500);
+        WiFi.off();
+        softDelay(4000);
+        digitalWrite(indicatorLED, HIGH); // leave on the indicator LED if this code is run
+
+        if (retries++ < CLOUD_MAXRETRY)
+        {
+            Log.info("Reconnect to Cloud (%d)", retries);
+
+            WiFi.off();
+            WiFi.on();
+            Particle.connect();
+
+            if (waitFor(Particle.connected, CLOUD_TIMEOUT))
+            { // only works with SYSTEM_THREAD(ENABLED)
+                retries = 0;
+            }
+            else
+            {
+                Log.info("\tfailed - retry in %d sec", CLOUD_TIMEOUT / 1000);
+                Particle.disconnect();
+            }
+
+            msSeenCloud = millis(); // don't check too early
+        }
+        else
+        {
+            System.reset(); // no success in reconnection - restart from scratch
+        }
+    }
 }
 
 // Loop
@@ -55,28 +156,47 @@ void loop()
     groupTemp = dallas.getTempCByIndex(0); // get the temperature of the Espresso Machine's group head
     caseTemp = dallas.getTempCByIndex(1);  // get the temperature of the Espresso Machine Case
 
-    if (Time.local() - timeLoop >= 3 && digitalRead(powerRelay) == HIGH && abs(groupTemp - tempLoop) > .1) // 3 second delay and 1/10 degree change
+    if (digitalRead(powerRelay) == HIGH)
     {
-        Particle.publish("Group Temp", String::format("%.2f", groupTemp), PRIVATE);
+        if (Time.local() - timeLoop >= 3 && abs(groupTemp - tempLoop) > .1) // 3 second delay and 1/10 degree change
+        {
+            Particle.publish("Group Temp", String::format("%.2f", groupTemp), PRIVATE);
 
-        tempLoop = groupTemp;
-        timeLoop = Time.local();
+            tempLoop = groupTemp;
+            timeLoop = Time.local();
+        }
+
+        if (Time.local() - timeOn > SHUTOFF_SEC) // Boiler safety shutoff
+        {
+            espressoShutoff();
+            Particle.publish("Safety", String::format("Timeout after %s minutes", timeOnMin), PRIVATE);
+        }
+        else if (caseTemp > SHUTOFF_TEMP)
+        {
+            espressoShutoff();
+            Particle.publish("Safety", String::format("Case Temp: %.2f ºC", caseTemp), PRIVATE);
+        }
+        else if (groupTemp > SHUTOFF_TEMP * 4)
+        {
+            espressoShutoff();
+            Particle.publish("Safety", String::format("Group Temp: %.2f ºC", groupTemp), PRIVATE);
+        }
+        else
+        {
+            /* TODO: PID goes here */
+        }
     }
 
-    if ((Time.local() - timeOn >= 14400) && (digitalRead(powerRelay) == HIGH)) // 4 hour boiler shutoff
+    if (Time.local() - lastSync > ONE_DAY_SEC)
     {
-        digitalWrite(boilerRelay, LOW);
-        Particle.publish("Boiler", "OFF", PRIVATE);
-    }
-
-    /* Request time synchronization from the Particle Cloud */
-    if (Time.now() % 86400 == 0)
-    {
-        Particle.syncTime();                                  // Request time synchronization from the Particle Device Cloud
-        waitUntil(Particle.syncTimeDone);                     // Wait until the device receives time from Particle Device Cloud (or connection to Particle Device Cloud is lost)
+        // Request time synchronization from the Particle Cloud
+        Particle.syncTime();
+        lastSync = Time.local();
+        waitUntil(Particle.syncTimeDone);                     // Wait here until the sync is complete
         Log.info("Current time: %s", Time.timeStr().c_str()); // Print current time
-        delay(1000);
     }
+
+    maintainWiFi();
 }
 
 // Functions
@@ -84,20 +204,12 @@ int espressoPower(String command)
 {
     if (command == "1")
     {
-        digitalWrite(powerRelay, HIGH);
-        delay(500);
-        digitalWrite(boilerRelay, HIGH);
-        timeOn = Time.local();
-        Particle.publish("Espresso", "ON", PRIVATE);
+        espressoStartup();
         return 1;
     }
     else if (command == "0")
     {
-        digitalWrite(boilerRelay, LOW);
-        // Particle.publish("Boiler", "OFF", PRIVATE);
-        delay(500);
-        digitalWrite(powerRelay, LOW);
-        Particle.publish("Espresso", "OFF", PRIVATE);
+        espressoShutoff();
         return 0;
     }
     else if (command == "?")
@@ -105,13 +217,11 @@ int espressoPower(String command)
         if (digitalRead(powerRelay) == HIGH)
         {
             digitalWrite(boilerRelay, HIGH);
-            Particle.publish("Espresso", "ON", PRIVATE);
             return 1;
         }
         else if (digitalRead(powerRelay) == LOW)
         {
             digitalWrite(boilerRelay, LOW);
-            Particle.publish("Espresso", "OFF", PRIVATE);
             return 0;
         }
         else
